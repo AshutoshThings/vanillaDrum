d91642d165c7cbf9e93bd336c9bafc378067b5ab
AshutoshThings <ashutoshv9648@gmail.com>
2025-07-04 16:40:06 +0530

    final commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
-window.addEventListener('keydown', (e)=>{
-	console.log(e.keyCode);
-});
+window.addEventListener('keydown', player);
+function player(e) {
+	let keyCode;
+	if (e.type === 'click') {
+		keyCode = e.currentTarget.getAttribute('data-key');
+	} else {
+		keyCode = e.keyCode;
+	}
+
+	const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+	const key = document.querySelector(`.key[data-key="${keyCode}"]`);
+	if (!audio) return;
+
+	audio.currentTime = 0;
+	audio.play();
+	key.classList.add('playing');
+}
+
+function removeTransition(e){
+	if(e.propertyName !== 'transform') return;
+	console.log(e.propertyName);
+	this.classList.remove('playing');
+}
+
+let keys = document.querySelectorAll(".box.key");
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+console.log(keys);
+
+keys.forEach(key => key.addEventListener('click', player));
